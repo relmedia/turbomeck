6a2a659a08b4a29b77b85a05483226288d399d2b
ariel7619 <olberg.ariel@gmail.com>
2025-01-19 21:17:55 +0100

    Update page.tsx

--- app/(client)/orders/page.tsx
+++ app/(client)/orders/page.tsx
@@ -1,10 +1,19 @@
 import { requiredUser } from "@/hooks/requiredUser";
+import { getMyOrders } from "@/sanity/helpers";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = async () => {
+const OrdersPage = async () => {
   await requiredUser();
-  const orders =await
-  return [];
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const orders = await getMyOrders(userId);
+  console.log(orders);
+
+  return <div>OrdersPage</div>;
 };
 
-export default page;
+export default OrdersPage;
