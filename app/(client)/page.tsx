b5efd8cbb366d531165ba077f8905b9a6c540a35
ariel7619 <olberg.ariel@gmail.com>
2025-01-21 22:42:03 +0100

    Update page.tsx

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,13 +1,13 @@
 import Container from "@/components/Container";
 import HeroSlide from "@/components/HeroSlide";
-import HomeBanner from "@/components/HomeBanner";
+//import HomeBanner from "@/components/HomeBanner";
 import ProductGrid from "@/components/ProductGrid";
 
 export default function Home() {
   return (
     <Container className="py-10">
       <HeroSlide />
-      <HomeBanner />
+      {/* <HomeBanner /> */}
       <ProductGrid />
     </Container>
   );
