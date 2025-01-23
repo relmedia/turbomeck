fac8c0ca159404b1d3845aea4bd0bf10ce9d2213
ariel7619 <olberg.ariel@gmail.com>
2025-01-23 23:44:43 +0100

    Update layout.tsx

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -62,7 +62,6 @@ export default async function RootLayout({
           <SanityLive />
           {/* <Chat pageId="106453562488245" appId="560672463472073" />
     <ChatIcon /> */}
-          <script src="https://cookiechimp.com/widget/XeEt53w.js"></script>
         </body>
       </html>
     </ClerkProvider>
