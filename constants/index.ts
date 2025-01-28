7929e7f8ce14b665f156bb6fc7397d8917561b55
ariel7619 <olberg.ariel@gmail.com>
2025-01-28 21:48:06 +0100

    hghghg

--- constants/index.ts
+++ constants/index.ts
@@ -11,7 +11,7 @@ export const productType = [
   { title: "Bransletryck", value: "bransletryck" },
   { title: "Downpipes", value: "downpipes" },
   { title: "Dumpventiler", value: "dumpventiler" },
-  { title: "Flansar och V-band", value: "flansar-v-band" },
+  { title: "V-band", value: "v-band" },
   { title: "Intercooler", value: "intercooler" },
   { title: "Renoveringssatser", value: "renoveringssatser" },
   { title: "Wastegate", value: "wastegate" },
--- sanity/schemaTypes/productType.ts
+++ sanity/schemaTypes/productType.ts
@@ -85,7 +85,7 @@ export const productType = defineType({
           { title: "Bransletryck", value: "bransletryck" },
           { title: "Downpipes", value: "downpipes" },
           { title: "Dumpventiler", value: "dumpventiler" },
-          { title: "Flansar och V-band", value: "flansar-v-band" },
+          { title: "V-band", value: "v-band" },
           { title: "Intercooler", value: "intercooler" },
           { title: "Renoveringssatser", value: "renoveringssatser" },
           { title: "Wastegate", value: "wastegate" },
