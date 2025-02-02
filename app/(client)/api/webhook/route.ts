355203de81c3b3538f816a5293908d382ed4e688
ariel7619 <olberg.ariel@gmail.com>
2025-02-02 23:54:08 +0100

    Update route.ts

--- app/(client)/api/webhook/route.ts
+++ app/(client)/api/webhook/route.ts
@@ -124,7 +124,7 @@ async function createOrderInSanity(
 
     products: sanityProducts,
     totalPrice: amount_total ? amount_total / 100 : 0,
-    status: "paid",
+    status: "PAID",
     orderDate: new Date().toISOString(),
     invoice: invoice
       ? {
